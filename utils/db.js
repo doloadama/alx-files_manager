92fa2909308317bc2adbf74ef9a9a4c0221129dd
doloadama <doloadama@outlook.com>
2023-06-22 16:50:28 +0000

    MongoDB utils

---
+++ utils/db.js
@@ -0,0 +1,40 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+
+    this.client = new MongoClient(`mongodb://${host}:${port}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    this.client.connect((err) => {
+      if (err) {
+        console.error('Database connection error:', err);
+      } else {
+        console.log('Database connection successful');
+      }
+    });
+
+    this.db = this.client.db(database);
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
