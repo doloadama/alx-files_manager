b4288cdc7047772ec919be6ad6067deb08230cca
doloadama <doloadama@outlook.com>
2023-06-26 09:56:23 +0000

    Usercontrol app

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,31 +1,39 @@
 import dbClient from '../utils/db';
-import crypto from 'crypto';
-
-const sha1 = (data) => crypto.createHash('sha1').update(data).digest('hex');
+import sha1 from 'sha1'
 
 
 class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
 
+    // Check if email is missing
     if (!email) {
       return res.status(400).json({ error: 'Missing email' });
     }
 
+    // Check if password is missing
     if (!password) {
       return res.status(400).json({ error: 'Missing password' });
     }
 
-    const userExists = await dbClient.getUser({ email });
-
+    // Check if email already exists in DB
+    const userExists = await dbClient.db.collection('users').findOne({ email });
     if (userExists) {
       return res.status(400).json({ error: 'Already exist' });
     }
 
+    // Hash the password using SHA1
     const hashedPassword = sha1(password);
-    const newUser = await dbClient.createUser({ email, password: hashedPassword });
 
-    return res.status(201).json({ id: newUser._id, email: newUser.email });
+    // Create a new user in the database
+    const newUser = await dbClient.db.collection('users').insertOne({
+      email,
+      password: hashedPassword,
+    });
+
+    // Return the new user with only email and id
+    const { _id, email: userEmail } = newUser.ops[0];
+    return res.status(201).json({ id: _id, email: userEmail });
   }
 }
 
