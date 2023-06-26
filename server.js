dff05639a894a81552d294f5c5096bfa3c38bd63
doloadama <doloadama@outlook.com>
2023-06-26 10:37:07 +0000

    .

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-import express from 'express';
-import routes from "./routes/index.js";
+const express = require('express');
+const routes = require("./routes/index.js");
+
+
 const app = express();
 const port = process.env.PORT || 5000;
 
